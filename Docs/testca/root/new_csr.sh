79b4ecc89ed187216d77bf09a608f03c122111ed
Joel Maslak <jmaslak@antelope.net>
2011-03-27 19:47:54 -0600

    Remove unneded files

--- Docs/testca/root/new_csr.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-false
-until [ $? -eq 0 ] ; do
-	openssl req -newkey rsa:2048 -nodes -keyout private/$1.pem \
-		-out requests/$1.csr -config openssl.cnf
-done
-
--- Docs/testca/root/print_crl.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-openssl crl -in rootcrl.pem -text -CAfile cacert.pem -noout 2>&1
